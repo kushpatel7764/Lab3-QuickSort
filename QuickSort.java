5d6baa5c286686bfb3711d703ccfcf6d46853464
Kush <94019918+kushpatel7764@users.noreply.github.com>
2022-11-14 13:35:55 -0500

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -5,7 +5,8 @@ class QuickSort{
     }
 
     public static void quicksort(int[] nums, int lowIndex, int highIndex){
-        if (lowIndex >= highIndex){
+        //Base Case: when looking at a sub-array of size 1 or less
+        if (lowIndex >= highIndex){ 
             return; 
         }
         
@@ -21,11 +22,20 @@ class QuickSort{
                 --rightPoint;
             }
 
-            swap(nums, leftPoint, rightPoint);
+            swap(nums, leftPoint, rightPoint); //switching out of place values
         }
-        
+        swap(nums, leftPoint, highIndex); //Swapping partition in
+
+
+        //Recursive calls
+        quicksort(nums, lowIndex, leftPoint-1); //quicksort the left half
+        quicksort(nums, leftPoint+1, highIndex); //quicksort the right half
     }
     public static void swap(int[] nums, int index1, int index2){
+            if(index1 == index2){
+                return; 
+            }
+            System.out.println("Swapping "+ nums[index1]+" and "+nums[index2]);
             int temp = nums[index1];
             nums[index1] = nums[index2];
             nums[index2] = temp; 
